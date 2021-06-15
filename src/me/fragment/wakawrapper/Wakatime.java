080c3494827e32ef5d2a8df0275e1c5da69b788e
Clément <clemcailly@gmail.com>
2021-06-15 19:47:31 +0200

    update gitignore & add main functions

--- src/me/fragment/wakawrapper/Wakatime.java
+++ src/me/fragment/wakawrapper/Wakatime.java
@@ -13,10 +13,56 @@ import me.fragment.wakawrapper.request.Request;
 
 public class Wakatime {
 
-	public static void main(String[] args) {
-		new Summaries(SummariesRange.Today).fetchData().thenAccept(summary -> {
-			System.out.println(summary.getProjects());
-		});
+	public static CompletableFuture<Summaries> getSummaries(SummariesRange range) {
+		CompletableFuture<Summaries> future = new CompletableFuture<Summaries>();
+		new Summaries(range).fetchData().thenAccept(summary -> future.complete(summary));
+
+		return future;
+	}
+
+	public static CompletableFuture<List<Project>> getProjectWithName(String name) {
+		CompletableFuture<List<Project>> future = new CompletableFuture<List<Project>>();
+
+		new Thread(() -> {
+			List<Project> projects = new ArrayList<Project>();
+
+			try {
+				JsonObject data = new Request().getRequest("projects", new HashMap<String, String>() {
+					private static final long serialVersionUID = 1L;
+					{
+						put("q", name);
+					}
+				});
+				data.get("data").getAsJsonArray().forEach(project -> {
+					projects.add(new Project(project.getAsJsonObject().get("name").getAsString(), Utils.getDateFromString(project.getAsJsonObject().get("created_at").getAsString()),
+							project.getAsJsonObject().get("url").getAsString()));
+				});
+
+				if (data.get("total_pages").getAsInt() > 1) {
+					while (data.get("page").getAsInt() < data.get("next_page").getAsInt()) {
+						int nextPage = data.get("next_page").getAsInt();
+
+						data = new Request().getRequest("projects", new HashMap<String, String>() {
+							private static final long serialVersionUID = 1L;
+							{
+								put("q", name);
+								put("page", String.valueOf(nextPage));
+							}
+						});
+						data.get("data").getAsJsonArray().forEach(project -> {
+							projects.add(new Project(project.getAsJsonObject().get("name").getAsString(),
+									Utils.getDateFromString(project.getAsJsonObject().get("created_at").getAsString()), project.getAsJsonObject().get("url").getAsString()));
+						});
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			future.complete(projects);
+		}).start();
+
+		return future;
 	}
 
 	public static CompletableFuture<List<Project>> getAllProjects() {
